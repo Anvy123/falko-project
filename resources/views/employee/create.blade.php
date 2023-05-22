77c2f5db902850efe05806c44b2695dbd04316a2
Anvy123 <kokooajaaa@gmail.com>
2023-05-22 23:28:38 +0700

    first commit

--- resources/views/employee/create.blade.php
+++ resources/views/employee/create.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,12 +8,15 @@
     <title>{{ $pageTitle }}</title>
     @vite('resources/sass/app.scss')
 </head>
+
 <body>
     <nav class="navbar navbar-expand-md navbar-dark bg-primary">
         <div class="container">
-            <a href="{{ route('home') }}" class="navbar-brand mb-0 h1"><i class="bi-hexagon-fill me-2"></i> Data Master</a>
+            <a href="{{ route('home') }}" class="navbar-brand mb-0 h1"><i class="bi-hexagon-fill me-2"></i> Data
+                Master</a>
 
-            <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">
+            <button type="button" class="navbar-toggler" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent">
                 <span class="navbar-toggler-icon"></span>
             </button>
 
@@ -21,12 +25,14 @@
 
                 <ul class="navbar-nav flex-row flex-wrap">
                     <li class="nav-item col-2 col-md-auto"><a href="{{ route('home') }}" class="nav-link">Home</a></li>
-                    <li class="nav-item col-2 col-md-auto"><a href="{{ route('employees.index') }}" class="nav-link">Employee List</a></li>
+                    <li class="nav-item col-2 col-md-auto"><a href="{{ route('employees.index') }}"
+                            class="nav-link">Employee List</a></li>
                 </ul>
 
                 <hr class="d-lg-none text-white-50">
 
-                <a href="{{ route('profile') }}" class="btn btn-outline-light my-2 ms-md-auto"><i class="bi-person-circle me-1"></i> My Profile</a>
+                <a href="{{ route('profile') }}" class="btn btn-outline-light my-2 ms-md-auto"><i
+                        class="bi-person-circle me-1"></i> My Profile</a>
             </div>
         </div>
     </nav>
@@ -45,40 +51,63 @@
                     <div class="row">
                         <div class="col-md-6 mb-3">
                             <label for="firstName" class="form-label">First Name</label>
-                            <input class="form-control @error('firstName') is-invalid @enderror" type="text" name="firstName" id="firstName" value="{{old('firstName')}}" placeholder="Enter First Name">
+                            <input class="form-control @error('firstName') is-invalid @enderror" type="text"
+                                name="firstName" id="firstName" value="{{ old('firstName') }}"
+                                placeholder="Enter First Name">
                             @error('firstName')
                                 <div class="text-danger">{{ $message }}</div>
-                                @enderror
+                            @enderror
                         </div>
                         <div class="col-md-6 mb-3">
                             <label for="lastName" class="form-label">Last Name</label>
-                            <input class="form-control  @error('lastName') is-invalid @enderror" type="text" name="lastName" id="lastName" value="{{old('lastName')}}" placeholder="Enter Last Name">
+                            <input class="form-control  @error('lastName') is-invalid @enderror" type="text"
+                                name="lastName" id="lastName" value="{{ old('lastName') }}"
+                                placeholder="Enter Last Name">
                             @error('lastName')
                                 <div class="text-danger">{{ $message }}</div>
-                                @enderror
+                            @enderror
                         </div>
                         <div class="col-md-6 mb-3">
                             <label for="email" class="form-label">Email</label>
-                            <input class="form-control @error('email') is-invalid @enderror" type="text" name="email" id="email" value="{{old('email')}}" placeholder="Enter Email">
+                            <input class="form-control @error('email') is-invalid @enderror" type="text"
+                                name="email" id="email" value="{{ old('email') }}" placeholder="Enter Email">
                             @error('email')
                                 <div class="text-danger">{{ $message }}</div>
-                                @enderror
+                            @enderror
                         </div>
                         <div class="col-md-6 mb-3">
                             <label for="age" class="form-label">Age</label>
-                            <input class="form-control @error('age') is-invalid @enderror" type="text" name="age" id="age" value="{{old('age')}}" placeholder="Enter Age">
+                            <input class="form-control @error('age') is-invalid @enderror" type="text" name="age"
+                                id="age" value="{{ old('age') }}" placeholder="Enter Age">
                             @error('age')
                                 <div class="text-danger">{{ $message }}</div>
-                                @enderror
+                            @enderror
                         </div>
+                        <div class="col-md-12 mb-3">
+                            <label for="position" class="form-label">Position</label>
+                            <select name="position" id="position" class="form-select">
+                                @foreach ($positions as $position)
+                                    <option value="{{ $position->id }}"
+                                        {{ old('position') == $position->id ? 'selected' : '' }}>
+                                        {{ $position->code . ' - ' . $position->name }}</option>
+                                @endforeach
+                            </select>
+                            @error('position')
+                                <div class="text-danger"><small>{{ $message }}</small></div>
+                            @enderror
+                        </div>
+
+
                     </div>
                     <hr>
                     <div class="row">
                         <div class="col-md-6 d-grid">
-                            <a href="{{ route('employees.index') }}" class="btn btn-outline-dark btn-lg mt-3"><i class="bi-arrow-left-circle me-2"></i> Cancel</a>
+                            <a href="{{ route('employees.index') }}" class="btn btn-outline-dark btn-lg mt-3"><i
+                                    class="bi-arrow-left-circle me-2"></i> Cancel</a>
                         </div>
                         <div class="col-md-6 d-grid">
-                            <button type="submit" class="btn btn-dark btn-lg mt-3"><i class="bi-check-circle me-2"></i> Save</button>
+                            <button type="submit" class="btn btn-dark btn-lg mt-3"><i class="bi-check-circle me-2"></i>
+                                Save</button>
                         </div>
                     </div>
                 </div>
@@ -88,4 +117,5 @@
 
     @vite('resources/js/app.js')
 </body>
+
 </html>
